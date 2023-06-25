b98e41efbe5eba3a0fea26dd5d87dfd0d84e6e02
river <laolei@forkway.cn>
2023-06-25 20:06:40 +0800

    chore: enhance change dark mode

--- src/hooks/useThemeToggle.ts
+++ src/hooks/useThemeToggle.ts
@@ -1,36 +1,39 @@
-import { useEffect, useState } from "react";
-import { useLocalStorage } from "@/hooks/useLocalStorage";
+import {useEffect, useState} from 'react';
+import {useLocalStorage} from '@/hooks/useLocalStorage';
+import {an} from 'vitest/dist/types-94cfe4b4';
 
 export const useThemeToggle = () => {
   const [isToggled, setToggle] = useState(false);
   const { setItem, getItem } = useLocalStorage();
   useEffect(() => {
-    getItem("theme")
-      .then((theme) => {
-        // console.log("theme",theme);
-        document.documentElement.classList.toggle("dark", theme === "dark");
-        document.documentElement.classList.add(theme);
-        setToggle(theme === "dark");
-      })
-      .catch(() => {
-        const colorSchema = window.matchMedia("(prefers-color-scheme: dark)");
-        const handleColorSchemaChange = () => {
-          document.documentElement.classList.toggle(
-            "dark",
-            colorSchema.matches
-          );
-          setToggle(colorSchema.matches);
-        };
-        handleColorSchemaChange(); // update toggle initially
-        colorSchema.addEventListener("change", handleColorSchemaChange);
-        return () => {
-          colorSchema.removeEventListener("change", handleColorSchemaChange);
-        };
-      });
+    getItem('theme').then(
+        (theme) => {
+          // console.log("theme",theme);
+          document.documentElement.classList.toggle('dark', theme === 'dark');
+          document.documentElement.classList.add(theme);
+          setToggle(theme === 'dark');
+        },
+    ).catch(
+        () => {
+          const colorSchema = window.matchMedia('(prefers-color-scheme: dark)');
+          const handleColorSchemaChange = () => {
+            document.documentElement.classList.toggle('dark', colorSchema.matches);
+            setToggle(colorSchema.matches);
+          };
+          handleColorSchemaChange(); // update toggle initially
+          colorSchema.addEventListener('change', handleColorSchemaChange);
+          return () => {
+            colorSchema.removeEventListener('change', handleColorSchemaChange);
+          };
+        },
+    );
+
+
   }, []);
 
-  const handleToggleClick = (event) => {
+  const handleToggleClick = () => {
     const element = document.documentElement;
+    const isDark = !element.classList.contains('dark');
     element.classList.add("animate");
 
     setTimeout(() => {
@@ -40,30 +43,25 @@ export const useThemeToggle = () => {
 
     setTimeout(() => element.classList.remove("animate"), 300);
 
-    // element.classList.toggle("dark", isDark);
-    setItem("theme", isDark ? "dark" : "light");
+    setItem('theme', isDark ? 'dark' : 'light');
     setToggle(isDark);
   };
 
-  /**
-   * 切换主题色，扩散渐变动画
-   *
-   * @param {MouseEvent} event 点击事件
-   */
-  function toggleTheme(event) {
-    const willDark = !isDark();
-
-    const transition = document.startViewTransition(() => {
-      toggleDark();
+  function toggleTheme(event:any) {
+    const element = document.documentElement;
+    const willDark = ! element.classList.contains('dark')
+    const doc = document as any
+    const transition = doc.startViewTransition(() => {
+      const isDark = !element.classList.contains('dark');
+      element.classList.toggle('dark', isDark);
     });
 
-    // 传入点击事件，从点击处开始扩散。否则，从右上角开始扩散
     const x = event?.clientX ?? window.innerWidth;
     const y = event?.clientY ?? 0;
 
     const endRadius = Math.hypot(
-      Math.max(x, innerWidth - x),
-      Math.max(y, innerHeight - y)
+        Math.max(x, innerWidth - x),
+        Math.max(y, innerHeight - y)
     );
     void transition.ready.then(() => {
       const clipPath = [
@@ -71,29 +69,18 @@ export const useThemeToggle = () => {
         `circle(${endRadius}px at ${x}px ${y}px)`,
       ];
       document.documentElement.animate(
-        {
-          clipPath: willDark ? clipPath : [...clipPath].reverse(),
-        },
-        {
-          duration: 500,
-          easing: "ease-in",
-          pseudoElement: willDark
-            ? "::view-transition-new(root)"
-            : "::view-transition-old(root)",
-        }
+          {
+            clipPath: willDark ? clipPath : [...clipPath].reverse(),
+          },
+          {
+            duration: 500,
+            easing: "ease-in",
+            pseudoElement: willDark
+                ? "::view-transition-new(root)"
+                : "::view-transition-old(root)",
+          }
       );
     });
-  }
-
-  /**
-   * 当前主题色是否是暗色
-   */
-  function isDark() {
-    return document.documentElement.classList.contains("dark");
-  }
-
-  function toggleDark() {
-    document.documentElement.classList.toggle("dark");
   }
 
   return {
