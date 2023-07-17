047e07d90a9ba9e12f356c829286d5bc91be79ba
river <laolei@forkway.cn>
2023-07-17 20:51:10 +0800

    chore: support vercel api

--- api/proxy.js
+++ api/proxy.js
@@ -9,7 +9,7 @@ module.exports = (req, res) => {
     // 代理目标地址
     // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
     if (req.url.startsWith('/mj-api')) { //这里使用/api可能会与vercel serverless 的 api 路径冲突，根据接口进行调整
-        const hostUrl = process.env.VITE_MIDJOURNEY_PROXY_URL;
+        const hostUrl = 'https://midjourney-proxy-production-1382.up.railway.app';
         console.log(hostUrl)
     }
     // 创建代理对象并转发请求
