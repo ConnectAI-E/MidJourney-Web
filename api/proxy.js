e7b0b98e3771ea00514e1da60774f0a92ec3e832
river <laolei@forkway.cn>
2023-07-17 21:11:54 +0800

    chore: support vercel api

--- api/proxy.js
+++ api/proxy.js
@@ -8,7 +8,7 @@ module.exports = (req, res) => {
     let target = ''
     // 代理目标地址
     if (req.url.startsWith('/mj-api')) { //这里使用/api可能会与vercel serverless 的 api 路径冲突，根据接口进行调整
-        target = process.env.VITE_MIDJOURNEY_PROXY_URL;
+        target = 'https://midjourney-proxy-production-1382.up.railway.app'
     }
     // 创建代理对象并转发请求
     createProxyMiddleware({
